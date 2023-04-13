0023fec7bc22fdcaca383b06bed98c62e438ff84
pcavendano <pedro.contreras.avendano@gmail.com>
2023-04-13 19:56:40 -0400

    Installation des service workers pour le projet frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -68,8 +68,14 @@ import {environment} from "../environments/environment";
     FormsModule,
     MatSnackBarModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', {
-    enabled: environment.production,
+    // ServiceWorkerModule.register('/ngsw-worker.js', {
+    // enabled: environment.production,
+    //   // Register the ServiceWorker as soon as the application is stable
+    //   // or after 30 seconds (whichever comes first).
+    //   registrationStrategy: 'registerWhenStable:30000'
+    // }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
